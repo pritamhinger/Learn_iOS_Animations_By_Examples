5314dd3126bacf53bbf371ec34b5a2ac02bd96cc
pritam hinger <pritamgenius24@gmail.com>
2017-03-25 23:25:01 +0530

    Complete all the code as explained in the chapter.

--- 08-getting-started-with-layer-animations/BahamaAirLoginScreen/ViewController.swift
+++ 08-getting-started-with-layer-animations/BahamaAirLoginScreen/ViewController.swift
@@ -80,10 +80,23 @@ class ViewController: UIViewController {
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
 
-    heading.center.x  -= view.bounds.width
-    username.center.x -= view.bounds.width
-    password.center.x -= view.bounds.width
-
+    let flyRight = CABasicAnimation(keyPath: "position.x")
+    flyRight.fromValue = -view.bounds.size.width/2
+    flyRight.toValue = view.bounds.size.width/2
+    flyRight.duration = 0.5
+    
+    
+    heading.layer.add(flyRight, forKey: nil)
+    
+    flyRight.fillMode = kCAFillModeBoth
+    flyRight.beginTime = CACurrentMediaTime() + 0.3
+    username.layer.add(flyRight, forKey: nil)
+    username.layer.position.x = view.bounds.size.width/2
+    
+    flyRight.beginTime = CACurrentMediaTime() + 0.5
+    password.layer.add(flyRight, forKey: nil)
+    password.layer.position.x = view.bounds.size.width/2
+    
     cloud1.alpha = 0.0
     cloud2.alpha = 0.0
     cloud3.alpha = 0.0
@@ -95,26 +108,6 @@ class ViewController: UIViewController {
 
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
-
-    UIView.animate(withDuration: 0.5) {
-      self.heading.center.x += self.view.bounds.width
-    }
-
-    UIView.animate(withDuration: 0.5, delay: 0.3, usingSpringWithDamping: 0.6,
-      initialSpringVelocity: 0.0,
-      animations: {
-        self.username.center.x += self.view.bounds.width
-      },
-      completion: nil
-    )
-
-    UIView.animate(withDuration: 0.5, delay: 0.4, usingSpringWithDamping: 0.6,
-      initialSpringVelocity: 0.0,
-      animations: {
-        self.password.center.x += self.view.bounds.width
-      },
-      completion: nil
-    )
 
     UIView.animate(withDuration: 0.5, delay: 0.5,
       animations: {
