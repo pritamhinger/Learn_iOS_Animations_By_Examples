d8fd9f94fc2882539c39fbce42f7efd72926332c
pritam hinger <pritamgenius24@gmail.com>
2017-04-01 23:35:15 +0530

    Add Replicating layer. Add particle to raplicate. Add animation to replicate the particle to change transform, opacity and backgroundColor to give impression of Twisting Wave.

--- 16-replicating-animations/Iris/ViewController.swift
+++ 16-replicating-animations/Iris/ViewController.swift
@@ -27,13 +27,39 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var meterLabel: UILabel!
     @IBOutlet weak var speakButton: UIButton!
+    let replicatorLayer = CAReplicatorLayer()
+    let dot = CALayer()
     
     let monitor = MicMonitor()
     let assistant = Assistant()
     
+    let dotLength: CGFloat = 6.0
+    let dotOffset: CGFloat = 8.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        replicatorLayer.frame = view.bounds
+        view.layer.addSublayer(replicatorLayer)
         
+        dot.frame = CGRect(x: replicatorLayer.frame.size.width - dotLength, y: replicatorLayer.position.y, width: dotLength, height: dotLength)
+        dot.backgroundColor = UIColor.lightGray.cgColor
+        dot.borderColor = UIColor(white: 1.0, alpha: 1.0).cgColor
+        dot.borderWidth = 0.5
+        dot.cornerRadius = 1.5
+        
+        replicatorLayer.addSublayer(dot)
+        
+        replicatorLayer.instanceCount = Int(view.frame.size.width / dotOffset)
+        replicatorLayer.instanceTransform = CATransform3DMakeTranslation(-dotOffset, 0.0, 0.0)
+        
+//        let move = CABasicAnimation(keyPath: "position.y")
+//        move.fromValue = dot.position.y
+//        move.toValue = dot.position.y - 50.0
+//        move.duration = 1.0
+//        move.repeatCount = 10
+//        dot.add(move, forKey: nil)
+        
+        replicatorLayer.instanceDelay = 0.02
         
     }
     
@@ -50,8 +76,35 @@ class ViewController: UIViewController {
     }
     
     func startSpeaking() {
-        print("speak back")
+        let scale = CABasicAnimation(keyPath: "transform")
+        scale.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
+        scale.toValue = NSValue(caTransform3D: CATransform3DMakeScale(1.4, 15, 1.0))
+        scale.duration = 0.33
+        scale.repeatCount = .infinity
+        scale.autoreverses = true
+        scale.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        dot.add(scale, forKey: "dotScale")
         
+        let fade = CABasicAnimation(keyPath: "opacity")
+        fade.fromValue = 1.0
+        fade.toValue = 0.2
+        fade.duration = 0.33
+        fade.beginTime = CACurrentMediaTime() + 0.33
+        fade.repeatCount = .infinity
+        fade.autoreverses = true
+        fade.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        dot.add(fade, forKey: "dotOpacity")
+        
+        let tint = CABasicAnimation(keyPath: "backgroundColor")
+        tint.fromValue = UIColor.magenta.cgColor
+        tint.toValue = UIColor.cyan.cgColor
+        tint.duration = 0.66
+        tint.beginTime = CACurrentMediaTime() + 0.28
+        tint.repeatCount = .infinity
+        tint.autoreverses = true
+        tint.fillMode = kCAFillModeBackwards
+        tint.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        dot.add(tint, forKey: "dotColor")
         
     }
     
